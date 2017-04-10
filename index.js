5aecae439f8d0ec1479b2ef8d8eace26c1794d01
Jonathan Louis Ng <jonathanlouisng@gmail.com>
2017-04-10 15:52:18 +0800

    add mlab security

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var app = express()
 var port = process.env.PORT || 4000
 
 // mongoose setup
-var dbURI = 'mongodb://admin:admin@ds157390.mlab.com:57390/mymdb'
+var dbURI = process.env.PROD_MONGODB || 'mongodb://localhost:27017/mymdb'
 var mongoose = require('mongoose')
 mongoose.connect(dbURI)
 
