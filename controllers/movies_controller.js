19b02767ea0d07d77e7ffadf3681c9b3e3fbf21f
Jonathan Louis Ng <jonathanlouisng@gmail.com>
2017-04-10 15:57:10 +0800

    refactor router

--- controllers/movies_controller.js
+++ controllers/movies_controller.js
@@ -5,16 +5,14 @@ var router = express.Router()
 // setup movie model
 var Movie = require('../models/movie')
 
-router.get('/movies', function (req, res) {
+router.route('/movies')
+.get(function (req, res) {
   Movie.find({}, function (err, allMovies) {
     if (err) res.send(err)
     res.send(allMovies)
   })
 })
-
-// creating new movies
-router.post('/movies', function (req, res, next) {
-  // create movie when we receive the post request
+.post(function (req, res, next) {
   var newMovie = new Movie(req.body)
   newMovie.save(function (err, newMovie) {
     if (err) res.send(err)
